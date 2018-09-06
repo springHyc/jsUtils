2c91a0205645bf61753edabaf0101cb00e077d4b
贺贺 <zhulinger520@163.com>
2018-09-06 20:21:44 +0800

    chore: 转码成功

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const lodash = require("lodash");
+
+// const jsUtils = lodash;
+
+const jsUtils = {
+  ...lodash,
+  isEmpty: value => {
+    if (lodash.isNumber(value)) {
+      return !lodash.isNumber(value);
+    } else {
+      return lodash.isEmpty(value);
+    }
+  },
+  // 验证是否为100的倍数
+  isHundred: value => {
+    return /^[0-9]*[0-9]$/i.test(value) && value % 100 === 0;
+  },
+  //验证数字，支持小数点
+  isDigit: value => {
+    return /^[0-9]+(\.[0-9]+)?$/.test(value);
+  },
+  //验证数字，不支持小数点
+  isInteger: value => {
+    return /^[0-9]+$/.test(value);
+  }
+};
+
+module.exports = jsUtils;
--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,7 @@
-"use strict";
+const jsUtils = require("..");
 
-var jsUtils = require("..");
-
-describe("测试jsUtils", function () {
-  it("isEmpty", function () {
+describe("测试jsUtils", () => {
+  it("isEmpty", () => {
     expect(jsUtils.isEmpty(0)).toBeFalsy();
     expect(jsUtils.isEmpty("")).toBeTruthy();
     expect(jsUtils.isEmpty("0")).toBeFalsy();
@@ -12,7 +10,7 @@ describe("测试jsUtils", function () {
     expect(jsUtils.isEmpty("ss")).not.toBeTruthy();
   });
 
-  it("验证是否为100的整数倍", function () {
+  it("验证是否为100的整数倍", () => {
     expect(jsUtils.isHundred(0)).toBeTruthy();
     expect(jsUtils.isHundred(101)).toBeFalsy();
     expect(jsUtils.isHundred(200)).toBeTruthy();
@@ -24,12 +22,14 @@ describe("测试jsUtils", function () {
     expect(jsUtils.isHundred("1200dd")).toBeFalsy();
   });
 
-  it("验证是否为数字，支持小数点的", function () {
+  it("验证是否为数字，支持小数点的", () => {
     expect(jsUtils.isDigit(0)).toBeTruthy();
     expect(jsUtils.isDigit(12)).toBeTruthy();
     expect(jsUtils.isDigit(0.34)).toBeTruthy();
     expect(jsUtils.isDigit(12.3)).toBeTruthy();
-    expect(jsUtils.isDigit(12.388888888888888888888888888888888888888888888)).toBeTruthy();
+    expect(
+      jsUtils.isDigit(12.388888888888888888888888888888888888888888888)
+    ).toBeTruthy();
     expect(jsUtils.isDigit("12")).toBeTruthy();
     expect(jsUtils.isDigit("12.34")).toBeTruthy();
     expect(jsUtils.isDigit("12.3.4.5")).toBeFalsy();
@@ -44,7 +44,7 @@ describe("测试jsUtils", function () {
     expect(jsUtils.isDigit(undefined)).toBeFalsy();
   });
 
-  it("验证是否为数字，不支持小数点", function () {
+  it("验证是否为数字，不支持小数点", () => {
     expect(jsUtils.isInteger(0)).toBeTruthy();
     expect(jsUtils.isInteger(100)).toBeTruthy();
     // expect(jsUtils.isInteger(100.0)).toBeFalsy();
