91b8fc92fb500d26f8b7ed7c754acf1e35af6daf
贺贺 <zhulinger520@163.com>
2018-09-10 17:19:19 +0800

    chore: 修改文件组织方式

--- src/index.js
+++ src/index.js
@@ -1,47 +1,35 @@
 const lodash = require("lodash");
 
-// const jsUtils = lodash;
-
-// jsUtils.isEmpty = value => {
-//   if (lodash.isNumber(value)) {
-//     return !lodash.isNumber(value);
-//   } else {
-//     return lodash.isEmpty(value);
-//   }
-// };
-
-// // 验证是否为100的倍数
-// jsUtils.isHundred = value => {
-//   return /^[0-9]*[0-9]$/i.test(value) && value % 100 === 0;
-// };
-
-// //验证数字，支持小数点
-// jsUtils.isDigit = value => {
-//   return /^[0-9]+(\.[0-9]+)?$/.test(value);
-// };
-// //验证数字，不支持小数点
-// jsUtils.isInteger = value => {
-//   return /^[0-9]+$/.test(value);
-// };
-
-const jsUtils = {
-  ...lodash,
-  isEmpty: value => {
-    if (lodash.isNumber(value)) {
-      return !lodash.isNumber(value);
-    } else {
-      return lodash.isEmpty(value);
-    }
-  },
-  isHundred: value => {
-    return /^[0-9]*[0-9]$/i.test(value) && value % 100 === 0;
-  },
-  isDigit: value => {
-    return /^[0-9]+(\.[0-9]+)?$/.test(value);
-  },
-  isInteger: value => {
-    return /^[0-9]+$/.test(value);
+class jsUtils extends lodash {
+  constructor() {
+    super();
   }
+}
+
+jsUtils.isEmpty = value => {
+  if (lodash.isNumber(value)) {
+    return !lodash.isNumber(value);
+  } else {
+    return lodash.isEmpty(value);
+  }
+};
+
+/**
+ * 验证是否为100的倍数
+ * @param {Number | string} value
+ * @returns {Boolean}
+ */
+jsUtils.isHundred = value => {
+  return /^[0-9]*[0-9]$/i.test(value) && value % 100 === 0;
+};
+
+//验证数字，支持小数点
+jsUtils.isDigit = value => {
+  return /^[0-9]+(\.[0-9]+)?$/.test(value);
+};
+//验证数字，不支持小数点
+jsUtils.isInteger = value => {
+  return /^[0-9]+$/.test(value);
 };
 
 module.exports = jsUtils;
